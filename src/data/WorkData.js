fa220205ad618313dd04476186d22111cfa25b97
Zhalalov2 <fill_flls@icloud.com>
2025-03-02 22:20:15 +0600

    my first commit

---
+++ src/data/WorkData.js
@@ -0,0 +1,59 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Job listing website",
+        description:"A Job listing website build in ReactJS. With searching and filtering functionalities.",
+        tags:["react","sass"],
+        
+        demo:"http://react-job-listing-website.codebucks27.vercel.app/",
+        
+    },
+    {
+        id:2,
+        name:"Sidebar Menu",
+        description:"A cool looking sidebar menu build using ReactJS and styledComponents.It has really smooth animations.",
+        tags:["react","styledComponents","react-router"],
+        
+        demo:"https://react-sidebar.vercel.app/",
+       
+    },{
+        id:3,
+        name:"Todo App(React + Redux)",
+        description:"A todo app build using Redux-toolkit and framer-motion for smooth animations.",
+        tags:["react","redux","framer-motion"],
+        
+        demo:"http://react-redux-todo-app-git-main-codebucks27.vercel.app/",
+        
+    },
+    {
+        id:4,
+        name:"Responsive Navigation Menu",
+        description:"A reponsive navbar menu component in ReactJS with react-router functionalities. Built from scratch.",
+        tags:["react","react-router","css"],
+        
+        demo:"http://react-responsive-navbar-codebucks27.vercel.app/",
+        
+    },
+    {
+        id:5,
+        name:"Test-landing-site",
+        description:"My pet project was made in 2021, when i was just starting frontend development.",
+        tags:["react","boostrap", "react-router", "react-hooks"],
+        
+        demo:"https://zhalalov2-landing.netlify.app/",
+        
+    },
+    {
+        id:6,
+        name:"My first website is about me",
+        description:"I made a portfolio website for the first time in 2020y.",
+        tags:["react","boostrap", "react-router", "react-hooks"],
+        
+        demo:"https://zhalalov2-portfolio.netlify.app/",
+        
+    },
+    
+  
+] 
